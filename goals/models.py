ef74e36b797a1f641234ddbc2e3ac4b467392d19
Yevhenii Korolikhin <yevhenii@devforth.io>
2023-05-01 10:49:24 +0300

    add category model

--- goals/models.py
+++ goals/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from core.models import User
+from django.utils import timezone
+from django.utils.text import slugify
 
-# Create your models here.
+class BaseModel(models.Model):
+    class Meta:
+        abstract = True
+
+    is_deleted = models.BooleanField(default=False)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.created_at = timezone.now()
+
+        self.updated_at = timezone.now()
+        
+        return super().save(*args, **kwargs)
+
+class GoalCategory(BaseModel):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=60)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title, allow_unicode=True)
+        
+        return super().save(*args, **kwargs)
