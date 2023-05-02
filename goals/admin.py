f65e9d3dc075689fa42654cb09395c017f3cb018
Yevhenii Korolikhin <yevhenii@devforth.io>
2023-05-02 15:24:57 +0300

    improve admin for goal

--- goals/admin.py
+++ goals/admin.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from .models import GoalCategory, Goal, GoalComment
 
 class GoalCategoryAdmin(admin.ModelAdmin):
-    list_display = ('title', 'user', 'created', 'updated')
-    search_fields = ('title', 'user')
+    list_display = ('id','title', 'user', 'is_deleted')
+    list_display_links = ('title', )
+    search_fields = ('title', )
+    list_filter = ('is_deleted', )
+    readonly_fields = ('created_at', 'updated_at')
 
 admin.site.register(GoalCategory, GoalCategoryAdmin) 
 admin.site.register(Goal)
